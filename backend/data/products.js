38bc44206359eb1ca2e7be123ad494a2964b4503
Rob <rhatcher94@outlook.com>
2021-01-01 18:19:20 -0500

    vid 29

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+	{
+		name: 'Dice Set Metalic Multi-Coloured',
+		image: '/images/dice1.jpg',
+		description:
+			'A multicolored dice set including with D20, D12, D10, D8, D6, D4.',
+		brand: 'Resin',
+		category: 'Dice Set',
+		price: 49.99,
+		countInStock: 10,
+		rating: 4.5,
+		numReviews: 12,
+	},
+	{
+		name: 'Dice Set Metalic Blue',
+		image: '/images/dice2.jpg',
+		description:
+			'A Blue dice set including with D20, D12, D10, D8, D6, D4.',
+		brand: 'Resin',
+		category: 'Dice Set',
+		price: 29.99,
+		countInStock: 7,
+		rating: 4.0,
+		numReviews: 8,
+	},
+	{
+		name: 'D20 Metalic Purple',
+		image: '/images/dice3.jpg',
+		description: 'A single Metalic purple D20.',
+		brand: 'Resin',
+		category: 'Single',
+		price: 9.99,
+		countInStock: 5,
+		rating: 3,
+		numReviews: 12,
+	},
+	{
+		name: 'Dice Set Metalic Purple',
+		image: '/images/dice4.jpg',
+		description:
+			'A Purple dice set including with D20, D12, D10, D8, D6, D4.',
+		brand: 'Resin',
+		category: 'Dice Set',
+		price: 29.99,
+		countInStock: 11,
+		rating: 5,
+		numReviews: 12,
+	},
+	{
+		name: 'Dice Set Metalic Orange',
+		image: '/images/dice5.jpg',
+		description:
+			'An Orange dice set including with D20, D12, D10, D8, D6, D4.',
+		brand: 'Resin',
+		category: 'Dice Set',
+		price: 29.99,
+		countInStock: 7,
+		rating: 3.5,
+		numReviews: 10,
+	},
+	{
+		name: 'Dice Set Metalic Green',
+		image: '/images/dice6.jpg',
+		description:
+			'A Green dice set including with D20, D12, D10, D8, D6, D4.',
+		brand: 'Resin',
+		category: 'Dice Set',
+		price: 29.99,
+		countInStock: 0,
+		rating: 4,
+		numReviews: 12,
+	},
+]
+
+export default products
